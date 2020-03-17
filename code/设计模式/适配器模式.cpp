b3cb134ee9d274a160ea4a936ddd9dc9baeefedc
DESKTOP-VQN03I8\JINGLONG <jinglong_email@163.com>
2020-03-17 09:29:00 +0800

    新增适配器模式代码实现

---
+++ code/设计模式/适配器模式.cpp
@@ -0,0 +1,20 @@
+﻿#include "适配器模式.h"
+
+void DuckAdapterTest::main()
+{
+    cout << "使用适配器之前----------------" << endl;
+
+    MallardDuck* duck = new MallardDuck();
+    duck->fly();
+    duck->quack();
+
+    WildTurkey* turkey = new WildTurkey();
+    turkey->fly();
+    turkey->gobble();
+
+    cout << "对turkey使用适配器之后----------------" << endl;
+
+    duck = new TurkeyAdapter(turkey);
+    duck->fly();
+    duck->quack();
+}
---
+++ code/设计模式/适配器模式.h
@@ -0,0 +1,60 @@
+﻿#include "global.h"
+
+// 目标接口：当前系统业务所期待的接口
+class MallardDuck{
+public:
+    MallardDuck(){};
+    ~MallardDuck(){};
+
+public:
+    virtual void fly(){
+        cout << "As a MallarDuck, I`m flying" << endl;
+    }
+    virtual void quack(){
+        cout << "As a MallarDuck, Quack" << endl;
+    }
+};
+
+// 适配者：被访问和被适配的现存组件库中的组件接口
+class WildTurkey{
+public:
+    WildTurkey(){};
+    ~WildTurkey(){};
+
+public:
+    void gobble(){
+        cout << "As a WildTurkey, Gobble gobble" << endl;
+    }
+    void fly(){
+        cout << "As a WildTurkey, I`m flying a short distance" << endl;
+    }
+};
+
+// 适配器：它是一个转换器，通过继承或者引用适配者的对象，将适配者接口转换成目标接口，让客户按照目标接口的格式访问适配者
+class TurkeyAdapter : public MallardDuck{
+public:
+    WildTurkey *turkeyAdaptee;
+    TurkeyAdapter(WildTurkey *turkey){
+        this->turkeyAdaptee = turkey;
+    }
+    ~TurkeyAdapter(){};
+
+public:
+    void quack(){
+        turkeyAdaptee->gobble();
+    }
+    
+    void fly(){
+        turkeyAdaptee->fly();
+    }
+};
+
+// 客户代码
+class DuckAdapterTest{
+public:
+    DuckAdapterTest(){};
+    ~DuckAdapterTest(){};
+
+public:
+    void main();
+};
